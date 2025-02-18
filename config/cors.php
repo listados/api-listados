dc9509f95733b9c9d9c5d7340892c5a579c8b810
Junior-Shyko <franciscoanto@gmail.com>
2025-02-17 23:28:51 -0300

    Add dominio 360 listados

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:7000'],
+    'allowed_origins' => ['https://360.listados.com.br'],
 
     'allowed_origins_patterns' => [],
 
